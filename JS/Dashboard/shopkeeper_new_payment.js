64a6086fe77d56e1b05cb6a9169b4ed00ec121b9
Mahera Furniturewala <82437476+MaheraFurniturewala@users.noreply.github.com>
2021-11-07 19:00:37 +0530

    ✨ add transactions portal

--- JS/Dashboard/shopkeeper_new_payment.js
+++ JS/Dashboard/shopkeeper_new_payment.js
@@ -1,93 +1,28 @@
+let shop_details = JSON.parse(localStorage.getItem('shop_details'));
+let shop_id = shop_details._id;
+let Name = document.getElementById("Name");
+Name.innerText = shop_details.name;
 
 
-    // $.ajax({
-    //     type: "get",
-    //     async: "false",
-    //     url: "http://localhost:3000/api/users/",
-    //     success: function (data) {
-    //       let all_users = data.data;
-    //        user = all_users.filter((user) => {
-    //        return user.phoneNum == inputVal;
-    //       });
-    //       return user;
-    //     },
-    //     error: function (error) {
-    //       console.log(error);
-    //     },
-
-    // });
-    // return user[0];
-
-    
-// const getUser = async function (){
-//     try{
-//         let inputVal = document.getElementById("userNumber").value;
-//         const res = await fetch("http://localhost:3000/api/users/",{
-//             method: "GET",
-//         })
-//         .then(resp => resp.json())
-//         .then(data => {
-//             let all_users = data.data;
-//              user = all_users.filter((u) => {
-//              return u.phoneNum == inputVal;
-//             });
-//             return user;
-//           }
-//         )
-//         console.log(res[0]);
-//         return res[0];
-//     }catch(err){
-//         console.log(err)
-//         throw err;
-//     }
-// }
-
-
-
-    
-
-    // let InitializePayment = async function () {
-    //     let InitializePaymentForm = $("#InitializePayment");
-        
-    //     InitializePaymentForm.submit(async function (e) {
-    //         e.preventDefault();
-    //         let id = await getUser()._id
-    //         $.ajax({
-    //       type: "post",
-    //       url: `http://localhost:3000/api/transaction/${id}`,
-    //       data: InitializePaymentForm.serialize(),
-    //       success: function (data) {
-    //           console.log(data.data);
-    //       },
-    //       error: function (error) {
-    //           console.log(error)
-    //           return;
-    //       },
-    //     });
-    //   });
-    // };
-
-    // {
-    //     let InitializePayment = async function () {
-    //       let paymentForm = $("#InitializePayment");
-      
-    //       paymentForm.submit(async function (e) {
-    //         e.preventDefault(paymentForm.serialize());
-    //         let id = await getUser()._id
-    //         console.log(id);
-      
-    //         $.ajax({
-    //           type: "post",
-    //           url: `http://localhost:3000/api/transaction/${id}`,
-    //           data: paymentForm.serialize(),
-    //           success: function (data) {
-    //             console.log(data.data);
-    //           },
-    //           error: function (error) {
-               
-    //           },
-    //         });
-    //       });
-    //     };
-    //     InitializePayment();
-    // }
+{
+    let InitializePayments = function () {
+      let InitializePaymentForm = $("#InitializePayment");
+  
+      InitializePaymentForm.submit(function (e) {
+        e.preventDefault(InitializePaymentForm.serialize());
+  
+        $.ajax({
+          type: "post",
+          url: `http://localhost:3000/api/transaction/${shop_id}`,
+          data: InitializePaymentForm.serialize(),
+          success: function (data) {
+            console.log(data.data);
+          },
+          error: function (error) {
+            console.log(error);
+          },
+        });
+      });
+    };
+    InitializePayments();
+}
